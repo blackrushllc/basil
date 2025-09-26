da6d68ebe6756085b3e345d32691925be728a474
Erik Olson <erik@Olson.host>
2025-09-25 21:04:19 -0400

    wip

---
+++ target/debug/basilc.d
@@ -0,0 +1,1 @@
+E:\Projects\Yore\basil\target\debug\basilc.exe: E:\Projects\Yore\basil\basilc\src\main.rs E:\Projects\Yore\basil\basilcore\common\src\lib.rs E:\Projects\Yore\basil\basilcore\lexer\src\lib.rs
---
+++ target/debug/deps/basilc.d
@@ -0,0 +1,5 @@
+E:\Projects\Yore\basil\target\debug\deps\basilc.d: basilc\src\main.rs
+
+E:\Projects\Yore\basil\target\debug\deps\basilc.exe: basilc\src\main.rs
+
+basilc\src\main.rs:
